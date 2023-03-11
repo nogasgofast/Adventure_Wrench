6328f92f0779347b645fc0b9b4501c55702f3d31
nogasgofast <nogasgofast@gmail.com>
2023-03-10 21:17:24 -0600

    saving some chanes i had on the windows-setup.py file. Not sure if they are important.

--- src/windows-setup.py
+++ src/windows-setup.py
@@ -1,19 +1,19 @@
 from cx_Freeze import setup, Executable
 from version import ver
+import sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': [], 'includes':['pony.orm.dbproviders.sqlite']}
+build_options = {'packages': [], 'excludes': [], 'includes':['pony']}
 
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('main.py', base=base, target_name = 'adventure_wrench')
+    Executable('main.py', base=base, target_name='adventure_wrench')
 ]
 
 setup(name='adventure_wrench',
-      version = ver,
-      description = '5E compatible initiative tracker and template builder',
-      options = {'build_exe': build_options},
-      executables = executables)
+      version=ver,
+      description='5E compatible initiative tracker and template builder',
+      options={'build_exe': build_options},
+      executables=executables)
